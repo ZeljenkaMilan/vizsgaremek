3fcd393b88ea67f8335b9904218d70ea05bd8901
Ruzsa Levente <ruzsalevente.contact@gmail.com>
2024-12-03 09:17:41 +0100

    Delete vizsgaremek/sql/rendelesek.sql

--- vizsgaremek/sql/rendelesek.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE rendelesek (
-    azon INT AUTO_INCREMENT PRIMARY KEY,
-    vasarlo_azon INT NOT NULL,
-    datum DATE NOT NULL,
-    osszesen INT NOT NULL,
-    FOREIGN KEY (vasarlo_azon) REFERENCES vasarlok(azon)
-);
