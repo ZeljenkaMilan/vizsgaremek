4ed1f030e285ab41f386dbe51a00286c5286cf1c
Ruzsa Levente <ruzsalevente.contact@gmail.com>
2024-12-03 09:18:01 +0100

    Delete vizsgaremek/sql/tetelek.sql

--- vizsgaremek/sql/tetelek.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tetelek (
-    azon INT AUTO_INCREMENT PRIMARY KEY,
-    rendeles_azon INT NOT NULL,
-    termek_azon INT NOT NULL,
-    mennyiseg INT NOT NULL,
-    FOREIGN KEY (rendeles_azon) REFERENCES rendelesek(azon),
-    FOREIGN KEY (termek_azon) REFERENCES termekek(azon)
-);
