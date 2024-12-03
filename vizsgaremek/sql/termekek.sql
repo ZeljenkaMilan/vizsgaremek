23f6a571b1b42853a78f6e944f1888f99a3144c9
Ruzsa Levente <ruzsalevente.contact@gmail.com>
2024-12-03 09:17:52 +0100

    Delete vizsgaremek/sql/termekek.sql

--- vizsgaremek/sql/termekek.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE termekek (
-    azon INT AUTO_INCREMENT PRIMARY KEY,
-    nev VARCHAR(100) NOT NULL,
-    leiras TEXT,
-    kategoria VARCHAR(20),
-    ar INT NOT NULL,
-    keszlet INT NOT NULL
-);
